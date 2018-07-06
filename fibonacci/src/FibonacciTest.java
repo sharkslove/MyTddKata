1e2f2efdba0f84cdccee517f993876da3295b97a
believein08 <2425194544@qq.com>
2018-07-06 14:38:51 +0800

    add fibonacci by tdd

---
+++ fibonacci/src/FibonacciTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class FibonacciTest {
+    @Test
+    public void testFibonacci(){
+        int cases[][] = {{0,0},{1,1},{2,1},{3,2}};
+        for (int i =0; i< cases.length; i++){
+            assertEquals(cases[i][1], Fibonacci.fib(cases[i][0]));
+        }
+    }
+}
